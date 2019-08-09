ab2e108e9cd323003629c4f05e35b187569579b1
Zehua-Chen <peterchen06242000@outlook.com>
2019-08-09 16:21:41 +0800

    Add Working But Timed Out Approach

--- longest-palindromic-substring/find_palindromic_substring.py
+++ longest-palindromic-substring/find_palindromic_substring.py
@@ -1,2 +1,41 @@
 def find_palindromic_substring(s: str) -> str:
-    return s
+
+    def is_palindrome(s: str, head: int, tail: int) -> bool:
+        count = tail - head + 1
+
+        # odd
+        if count % 2 != 0:
+            while head != tail:
+                if s[head] == s[tail]:
+                    tail -= 1
+                    head += 1
+                else:
+                    return False
+            return True
+
+        # even
+        while (head - tail) != 1:
+            if s[head] == s[tail]:
+                head += 1
+                tail -= 1
+            else:
+                return False
+
+        return s[head] == s[tail]
+
+    max_head = 0
+    max_tail = 0
+    max_count = 0
+
+    for current_head in range(0, len(s)):
+        for current_tail in range(current_head, len(s)):
+            if is_palindrome(s, current_head, current_tail):
+                # determine if this palindrome is bigger than the one
+                # previously recorded
+                count = current_tail - current_head + 1
+                if count > max_count:
+                    max_count = count
+                    max_head = current_head
+                    max_tail = current_tail
+
+    return s[max_head:max_tail + 1]
--- longest-palindromic-substring/test.py
+++ longest-palindromic-substring/test.py
@@ -3,9 +3,12 @@ from find_palindromic_substring import find_palindromic_substring
 
 
 class FindLongestPalindromicSubstringTests(unittest.TestCase):
-    def test_given(self):
+    def test_given(self) -> None:
         self.assertEqual(find_palindromic_substring("babad"), "bab")
         self.assertEqual(find_palindromic_substring("cbbd"), "bb")
+
+    def test_nested(self) -> None:
+        self.assertEqual(find_palindromic_substring("abacdcaba"), "abacdcaba")
 
 if __name__ == "__main__":
     unittest.main()
