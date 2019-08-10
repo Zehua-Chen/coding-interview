311152e6993a633659b66a7b910e339ef9293a34
Zehua-Chen <peterchen06242000@outlook.com>
2019-08-10 18:38:02 +0800

    Complete

--- zigzag-conversion/convert.py
+++ zigzag-conversion/convert.py
@@ -1,2 +1,34 @@
 def convert(s: str, numRows: int) -> str:
-    return ""
+    # for y to be equal to numRows - 1, numRows must be bigger than 1
+    if numRows <= 1:
+        return s
+
+    # prepare output buffer
+    buffer = ""
+    row_buffers = list()
+
+    for i in range(0, numRows):
+        row_buffers.append("")
+
+    # populate buffer's content
+
+    step = 1
+    y = 0
+
+    for c in s:
+        row_buffers[y] += c
+
+        y += step
+
+        if y == 0:
+            step = 1
+        elif y == numRows - 1:
+            step = -1
+
+    for row_buffer in row_buffers:
+        buffer += row_buffer
+
+    return buffer
+
+if __name__ == "__main__":
+    print(list(range(0, 10)))
--- zigzag-conversion/test.py
+++ zigzag-conversion/test.py
@@ -7,6 +7,15 @@ class ZigZagConversionTests(unittest.TestCase):
         self.assertEqual(convert("PAYPALISHIRING", 3), "PAHNAPLSIIGYIR")
         self.assertEqual(convert("PAYPALISHIRING", 4), "PINALSIGYAHRPI")
 
+    def test_single_row(self):
+        self.assertEqual(convert("abcd", 1), "abcd")
+
+    def test_two_rows(self):
+        self.assertEqual(convert("abcdef", 2), "acebdf")
+
+    def test_empty(self):
+        self.assertEqual(convert("", 3), "")
+
 
 if __name__ == "__main__":
     unittest.main()
