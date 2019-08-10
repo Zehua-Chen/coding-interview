bcb90f9db7ab2e69e67a21f5c66c4265dbbd1009
Zehua-Chen <peterchen06242000@outlook.com>
2019-08-10 18:39:09 +0800

    Add Typing Info

--- zigzag-conversion/test.py
+++ zigzag-conversion/test.py
@@ -3,17 +3,17 @@ from convert import convert
 
 
 class ZigZagConversionTests(unittest.TestCase):
-    def test_given(self):
+    def test_given(self) -> None:
         self.assertEqual(convert("PAYPALISHIRING", 3), "PAHNAPLSIIGYIR")
         self.assertEqual(convert("PAYPALISHIRING", 4), "PINALSIGYAHRPI")
 
-    def test_single_row(self):
+    def test_single_row(self) -> None:
         self.assertEqual(convert("abcd", 1), "abcd")
 
-    def test_two_rows(self):
+    def test_two_rows(self) -> None:
         self.assertEqual(convert("abcdef", 2), "acebdf")
 
-    def test_empty(self):
+    def test_empty(self) -> None:
         self.assertEqual(convert("", 3), "")
 
 
