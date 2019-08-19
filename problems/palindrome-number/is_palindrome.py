956ab10fe37e46e7b33929f550581713a3edde9a
Zehua-Chen <peterchen06242000@outlook.com>
2019-08-19 22:58:19 +0800

    Implement Palindrome Number

--- problems/palindrome-number/is_palindrome.py
+++ problems/palindrome-number/is_palindrome.py
@@ -1,2 +1,26 @@
 def is_palindrome(x: int) -> bool:
-    return False
+    if x < 0:
+        return False
+
+    digits = []
+
+    while x >= 10:
+        digit = x % 10
+        x -= digit
+        x = int(x / 10)
+        digits.append(digit)
+
+    digits.append(x)
+
+    head_index = 0
+    tail_index = len(digits) - 1
+
+    while head_index != tail_index and (head_index + 1) != tail_index:
+
+        if digits[head_index] != digits[tail_index]:
+            return False
+
+        tail_index -= 1
+        head_index += 1
+
+    return digits[head_index] == digits[tail_index]
--- problems/palindrome-number/test.py
+++ problems/palindrome-number/test.py
@@ -8,6 +8,11 @@ class PalindromeNumberTests(unittest.TestCase):
         self.assertFalse(is_palindrome(-121))
         self.assertFalse(is_palindrome(10))
 
+    def test_even_palindrome(self):
+        self.assertTrue(is_palindrome(1221))
+        self.assertFalse(is_palindrome(1231))
+        self.assertFalse(is_palindrome(-1221))
+
     def test_zero(self):
         self.assertTrue(is_palindrome(0))
 
