1bc11b77a7475a08dc358c7715f6c507a80b529c
Zehua-Chen <peterchen06242000@outlook.com>
2019-08-25 09:05:40 -0500

    Add Solution

--- problems/romans-to-int/test.py
+++ problems/romans-to-int/test.py
@@ -7,8 +7,12 @@ class RomansToInteger(unittest.TestCase):
         self.assertEqual(convert("III"), 3)
         self.assertEqual(convert("IV"), 4)
         self.assertEqual(convert("IX"), 9)
-        self.assertEqual(convert("LVIII"), 48)
+        self.assertEqual(convert("LVIII"), 58)
         self.assertEqual(convert("MCMXCIV"), 1994)
+
+    def test_edge(self):
+        self.assertEqual(convert("I"), 1)
+        self.assertEqual(convert("MMMCMXCIX"), 3999)
 
 if __name__ == "__main__":
     unittest.main()
