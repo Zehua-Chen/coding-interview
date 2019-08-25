2decd440848bb27f2b42346c55bc89304115ca0f
Zehua-Chen <peterchen06242000@outlook.com>
2019-08-25 06:29:57 -0500

    Working but Slow Solution

--- problems/container-most-water/max_area.py
+++ problems/container-most-water/max_area.py
@@ -2,4 +2,16 @@ from typing import List
 
 
 def max_area(height: List[int]) -> int:
-    return 0
+    max_area = 0
+
+    for first_i, first_v in enumerate(height):
+        for second_i, second_v in enumerate(height):
+            w = second_i - first_i
+            h = first_v if first_v < second_v else second_v
+
+            area = w * h
+
+            if max_area < area:
+                max_area = area
+
+    return max_area
