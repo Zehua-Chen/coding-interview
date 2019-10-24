e96de06b5ffaa065bab709f7ba943df9750f00ec
Zehua-Chen <peterchen06242000@outlook.com>
2019-10-23 23:06:41 -0500

    [feat] Add Solutions

--- problems/three-sum-closest/main.cc
+++ problems/three-sum-closest/main.cc
@@ -1,5 +1,14 @@
+#include <iostream>
 #include "solution.h"
 
+using std::cout;
+using std::endl;
+
 int main() {
+  Solution solution;
+  // sorted: -4 -1 1 2
+  std::vector<int> nums{ -1, 2, 1, -4 };
+
+  cout << solution.ThreeSumClosest(nums, 1) << endl;
   return 0;
 }
--- problems/three-sum-closest/solution.cc
+++ problems/three-sum-closest/solution.cc
@@ -1,7 +1,50 @@
+#include <algorithm>
+#include <iterator>
+#include <cmath>
+#include <iostream>
 #include "solution.h"
 
 using std::vector;
 
+static int Difference(int a, int b) {
+  return std::abs(a - b);
+}
+
 int Solution::ThreeSumClosest(vector<int> &nums, int target) {
-  return 0;
+  std::sort(nums.begin(), nums.end());
+
+  // sample array
+  // -1 -1 2 4
+
+  auto end = nums.end();
+  auto i = nums.begin();
+
+  int best_value = *i + *std::next(i) + *std::prev(end);
+  int best_difference = Difference(best_value, target);
+
+  for (; i < end; ++i) {
+    auto j = std::next(i);
+    auto k = std::prev(end);
+
+    while (j < k) {
+      int temp_value = *i + *j + *k;
+      int temp_distance = temp_value - target;
+      int temp_difference = Difference(temp_value, target);
+
+      if (temp_difference < best_difference) {
+        best_value = temp_value;
+        best_difference = temp_difference;
+      }
+
+      if (temp_distance < 0) {
+        j = std::next(j);
+      } else if (temp_distance > 0) {
+        k = std::prev(k);
+      } else if (temp_distance == 0) {
+        return target;
+      }
+    }
+  }
+
+  return best_value;
 }
