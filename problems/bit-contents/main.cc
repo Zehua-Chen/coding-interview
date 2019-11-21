b9f2d5ccb4ecf5f13978f7ac991dcccfdf2c4666
Zehua-Chen <peterchen06242000@outlook.com>
2019-11-20 20:28:47 -0600

    [feat] Implement Bit Contents

--- problems/bit-contents/main.cc
+++ problems/bit-contents/main.cc
@@ -1,7 +1,17 @@
 #include <iostream>
 #include "solution.h"
 
+using std::cout;
+using std::endl;
+
 int main() {
+  DumpContent(static_cast<char>(0xFA));
+  cout << endl;
+
+  DumpContent(0x1);
+  cout << endl;
+
   DumpContent(0xFA);
+
   return 0;
 }
--- problems/bit-contents/solution.h
+++ problems/bit-contents/solution.h
@@ -2,15 +2,44 @@
 
 #include <iostream>
 #include <ostream>
+#include <string>
+#include <algorithm>
 
 // Declaration
 
 template <typename T>
 void DumpContent(const T &value, std::ostream &out = std::cout);
+void DumpByte(unsigned char byte, std::ostream &out = std::cout);
 
 // Implementation
 
 template <typename T>
 void DumpContent(const T &value, std::ostream &out) {
-  return;
+  const char *bytes = reinterpret_cast<const char *>(&value);
+
+  for (size_t i = 0; i < sizeof(value); ++i) {
+    out << i << ": ";
+    DumpByte(bytes[i], out);
+    out << std::endl;
+  }
+}
+
+void DumpByte(unsigned char byte, std::ostream &out) {
+  char byte_dump[9];
+
+  for (size_t i = 0; i < 8; ++i) {
+    unsigned char bit = byte & 0x1;
+    byte >>= 1;
+
+    if (bit) {
+      byte_dump[i] = '1';
+    } else {
+      byte_dump[i] = '0';
+    }
+  }
+
+  byte_dump[8] = 0;
+  std::reverse(std::begin(byte_dump), std::end(byte_dump) - 1);
+
+  out << byte_dump;
 }
