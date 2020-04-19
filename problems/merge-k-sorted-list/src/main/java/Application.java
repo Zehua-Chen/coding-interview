e4e99a8aad24ea6f04998cab43793ade4c3d6feb
Zehua-Chen <peterchen06242000@outlook.com>
2020-04-19 03:45:46 -0500

    [feat] Setup Java

---
+++ problems/merge-k-sorted-list/src/main/java/Application.java
@@ -0,0 +1,9 @@
+public class Application {
+    public static void test() {
+        Solution solution = new Solution();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ problems/merge-k-sorted-list/src/main/java/Solution.java
@@ -0,0 +1,5 @@
+public class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        return null;
+    }
+}
