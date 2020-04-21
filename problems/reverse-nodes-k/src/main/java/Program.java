7e203e27203d675f3b27072792ca30b4b8321e31
Zehua-Chen <peterchen06242000@outlook.com>
2020-04-21 14:23:45 -0500

    [feat] Setup Reverse Nodes in K Group

---
+++ problems/reverse-nodes-k/src/main/java/Program.java
@@ -0,0 +1,7 @@
+import utils.directed.ListNode;
+
+public class Program {
+  public static void main(String[] args) {
+    System.out.println(ListNode.create(1, 2, 3));
+  }
+}
---
+++ problems/reverse-nodes-k/src/main/java/Solution.java
@@ -0,0 +1,7 @@
+import utils.directed.ListNode;
+
+public class Solution {
+  public ListNode reverseKGroup(ListNode head, int k) {
+    return null;
+  }
+}
