5b8c61e80f4842a384bda2aa392f1caffac38d4c
Zehua-Chen <peterchen06242000@outlook.com>
2020-04-21 01:05:15 -0500

    [feat] Setup Swap Nodes In Pairs

---
+++ problems/swap-nodes-pairs/src/main/java/Program.java
@@ -0,0 +1,6 @@
+class Program {
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    System.out.println(solution.swapPairs(null));
+  }
+}
---
+++ problems/swap-nodes-pairs/src/main/java/Solution.java
@@ -0,0 +1,5 @@
+class Solution {
+  public ListNode swapPairs(ListNode head) {
+    return null;
+  }
+}
