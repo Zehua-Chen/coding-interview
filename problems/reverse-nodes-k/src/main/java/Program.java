4440e248ec1fa1378ec591f561147a57d96a0bfd
Zehua-Chen <peterchen06242000@outlook.com>
2020-04-21 20:34:42 -0500

    [feat] Implement reverse nodes k

--- problems/reverse-nodes-k/src/main/java/Program.java
+++ problems/reverse-nodes-k/src/main/java/Program.java
@@ -1,7 +1,19 @@
 import utils.directed.ListNode;
 
 public class Program {
+  public static void test(int k, ListNode head, ListNode expected) {
+    Solution solution = new Solution();
+    ListNode actual = solution.reverseKGroup(head, k);
+
+    if (actual == null || !expected.equals(actual)) {
+      System.out.printf(
+        "failed, k %d expects %s, actual %s\n", k, expected, actual);
+    }
+  }
+
   public static void main(String[] args) {
-    System.out.println(ListNode.create(1, 2, 3));
+    test(1, ListNode.create(1, 2, 3, 4, 5), ListNode.create(1, 2, 3, 4, 5));
+    test(2, ListNode.create(1, 2, 3, 4, 5), ListNode.create(2, 1, 4, 3 ,5));
+    test(3, ListNode.create(1, 2, 3, 4, 5), ListNode.create(3, 2, 1, 4 ,5));
   }
 }
--- problems/reverse-nodes-k/src/main/java/Solution.java
+++ problems/reverse-nodes-k/src/main/java/Solution.java
@@ -1,7 +1,55 @@
 import utils.directed.ListNode;
 
 public class Solution {
+  private boolean hasLength(ListNode head, int k) {
+    assert k > 0;
+
+    if (head == null) {
+      return false;
+    }
+
+    ListNode current = head;
+
+    for (int i = 1; i < k; i++) {
+      current = current.next;
+
+      if (current == null) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
   public ListNode reverseKGroup(ListNode head, int k) {
-    return null;
+    if (k <= 1) {
+      return head;
+    }
+
+    if (head == null) {
+      return null;
+    }
+
+    if (!hasLength(head, k)) {
+      return head;
+    }
+
+    ListNode first = head;
+    ListNode tail = head;
+    ListNode current = head.next;
+
+    for (int i = 1; i < k; i++) {
+      ListNode next = current.next;
+      current.next = first;
+      first = current;
+
+      current = next;
+    }
+
+    // based on how we change current, by this point, current would be
+    // the next of kth node
+    tail.next = reverseKGroup(current, k);
+
+    return first;
   }
 }
