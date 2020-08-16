d3c85d74c48de9b2911dfc7c1772048adb9633ec
Zehua-Chen <peterchen06242000@outlook.com>
2020-08-16 00:51:50 -0500

    [chore] Update gitignore(s)

--- leetcode-1-100/next-permutation/src/main/java/Solution.java
+++ leetcode-1-100/next-permutation/src/main/java/Solution.java
@@ -1,5 +1,10 @@
+import java.util.Arrays;
+
 public class Solution {
   public void nextPermutation(int[] nums) {
-
+    // idea
+    // go from back to head to find numbers that are not in order
+    // swap with back
+    // sort forward (Arrays.sort())
   }
 }
