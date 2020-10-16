a762afb9959ab10b45d3a0d41379fed63e07b9e3
Zehua-Chen <peterchen06242000@outlook.com>
2020-10-15 23:42:36 -0500

    [feat] Add Slow Solution (but Correct)

--- leetcode-1-100/substr-with-concat-all-words/src/main/java/Program.java
+++ leetcode-1-100/substr-with-concat-all-words/src/main/java/Program.java
@@ -1,35 +1,37 @@
 import java.util.*;
 
 public class Program {
-    public static void test(String s, String[] words, int[] expected) {
+    public static void test(String s, String[] words, Integer[] expected) {
         var solution = new Solution();
 
         List<Integer> actualList = solution.findSubstring(s, words);
         assert actualList != null;
 
         var actualSet = new HashSet<>(actualList);
+        var expectedSet = new HashSet<>(Arrays.asList(expected));
 
-        var expectedSet = new HashSet<Integer>();
-
-        for (int e : expected) {
-            expectedSet.add(e);
-        }
-
-        if (!actualSet.equals(expectedSet)) {
+        if (!actualSet.equals(expectedSet) || actualList.size() != expected.length) {
             System.out.printf(
                     "expected = %s, actual = %s\n",
-                    Arrays.toString(expectedSet.toArray()), Arrays.toString(actualSet.toArray()));
+                    Arrays.toString(expected), Arrays.toString(actualList.toArray()));
         }
     }
 
     public static void main(String[] args) {
-        test("barfoothefoobarman", new String[] {"foo", "bar"}, new int[] {0, 9});
+        test("barfoothefoobarman", new String[] {"foo", "bar"}, new Integer[] {0, 9});
 
         test(
                 "wordgoodgoodgoodbestword",
                 new String[] {"word", "good", "best", "word"},
-                new int[] {});
+                new Integer[] {});
 
-        test("barfoofoobarthefoobarman", new String[] {"bar", "foo", "the"}, new int[] {6, 9, 12});
+        test("barfoofoobarthefoobarman", new String[] {"bar", "foo", "the"}, new Integer[] {6, 9, 12});
+
+        test(
+                "wordgoodgoodgoodbestword",
+                new String[] {"word", "good", "best", "good"},
+                new Integer[] {8});
+
+        test("aaa", new String[] {"a", "a"}, new Integer[] {0, 1});
     }
 }
--- leetcode-1-100/substr-with-concat-all-words/src/main/java/Solution.java
+++ leetcode-1-100/substr-with-concat-all-words/src/main/java/Solution.java
@@ -1,16 +1,18 @@
 import java.util.*;
 
 public class Solution {
-    public static List<String> combinations(Set<String> words) {
+    public static List<String> combinations(List<String> words) {
         if (words.size() == 1) {
             return List.of(words.stream().findFirst().get());
         }
 
         var output = new ArrayList<String>();
-        var wordsCopy = new HashSet<>(words);
 
-        for (String word : words) {
-            wordsCopy.remove(word);
+        for (var i = 0; i < words.size(); i++) {
+            String word = words.get(i);
+            var wordsCopy = new ArrayList<>(words);
+            wordsCopy.remove(i);
+
             List<String> children = combinations(wordsCopy);
 
             for (String child : children) {
@@ -29,22 +31,21 @@ public class Solution {
 
         while (current != -1) {
             list.add(current);
-            current = s.indexOf(segment, current + segment.length());
+            current = s.indexOf(segment, current + 1);
         }
 
         return list;
     }
-    
+
     public List<Integer> findSubstring(String s, String[] words) {
-        List<String> segments = combinations(new HashSet<>(Arrays.asList(words)));
-        var output = new ArrayList<Integer>();
+        List<String> segments = combinations(Arrays.asList(words));
+        var output = new HashSet<Integer>();
 
         for (String segment : segments) {
-            System.out.println(segment);
             List<Integer> found = all(s, segment);
             output.addAll(found);
         }
 
-        return output;
+        return new ArrayList<>(output);
     }
 }
