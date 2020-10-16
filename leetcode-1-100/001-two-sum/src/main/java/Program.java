c78ecc7856e3852377bc9f1bbcef93b8e1c385e4
Zehua-Chen <peterchen06242000@outlook.com>
2020-10-16 14:05:14 -0500

    [refactor] Prefix Java Solutions with 00

---
+++ leetcode-1-100/001-two-sum/src/main/java/Program.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Program {
+    public static void test(int[] nums, int target, int[] expected) {
+        var solution = new Solution();
+        var actualSet = new HashSet<Integer>();
+        var expectedSet = new HashSet<Integer>();
+
+        int[] actualArray = solution.twoSum(nums, target);
+
+        assert actualArray != null;
+
+        for (int i : actualArray) {
+            actualSet.add(i);
+        }
+
+        for (int i : expected) {
+            expectedSet.add(i);
+        }
+
+        if (!actualSet.equals(expectedSet)) {
+            System.out.printf(
+                    "numbers = %s, expected = %s, actual = %s\n",
+                    Arrays.toString(nums),
+                    Arrays.toString(expected), Arrays.toString(actualArray));
+        }
+    }
+
+    public static void main(String[] args) {
+        test(new int[]{2, 3, 4, 10}, 7, new int[]{1, 2});
+        test(new int[]{3, 2, 4}, 6, new int[]{1, 2});
+        test(new int[]{-1, -2, -3, -4, -5}, -8, new int[]{2, 4});
+        test(new int[]{-1, 2, -3, 4, -6}, 3, new int[]{0, 3});
+        test(new int[]{3, 3}, 6, new int[]{0, 1});
+    }
+}
---
+++ leetcode-1-100/001-two-sum/src/main/java/Solution.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // a map from numbers to their indices in the array
+        var indices = new HashMap<Integer, Integer>();
+
+        for (var i = 0; i < nums.length; i++) {
+            indices.put(nums[i], i);
+        }
+
+        for (var i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int diff = target - num;
+
+            if (indices.containsKey(diff) && indices.get(diff) != i) {
+                return new int[]{i, indices.get(diff)};
+            }
+        }
+
+        return new int[]{0, 0};
+    }
+}
