353339adccad7173592b4e879493b797205260bb
Zehua-Chen <peterchen06242000@outlook.com>
2020-10-16 00:56:44 -0500

    [feat] Finish

--- leetcode-1-100/substr-with-concat-all-words/src/main/java/Solution.java
+++ leetcode-1-100/substr-with-concat-all-words/src/main/java/Solution.java
@@ -1,51 +1,52 @@
 import java.util.*;
 
 public class Solution {
-    public static List<String> combinations(List<String> words) {
-        if (words.size() == 1) {
-            return List.of(words.stream().findFirst().get());
-        }
-
-        var output = new ArrayList<String>();
-
-        for (var i = 0; i < words.size(); i++) {
-            String word = words.get(i);
-            var wordsCopy = new ArrayList<>(words);
-            wordsCopy.remove(i);
-
-            List<String> children = combinations(wordsCopy);
-
-            for (String child : children) {
-                output.add(word + child);
+    /**
+     * Determine if a substring contains each of the words
+     * @param substring the substring to inspect
+     * @param words the words to search for
+     * @param result if each word is present, if words[i] is present, result.get(i) is true
+     */
+    public void contains(String substring, String[] words, List<Boolean> result) {
+        for (var i = 0; i < words.length; i++) {
+            if (result.get(i)) {
+                continue;
             }
 
-            wordsCopy.add(word);
+            String word = words[i];
+
+            if (!substring.startsWith(word)) {
+                continue;
+            }
+
+            result.set(i, true);
+
+            contains(substring.substring(word.length()), words, result);
+            break;
         }
-
-        return output;
-    }
-
-    public static List<Integer> all(String s, String segment) {
-        var list = new ArrayList<Integer>();
-        var current = s.indexOf(segment);
-
-        while (current != -1) {
-            list.add(current);
-            current = s.indexOf(segment, current + 1);
-        }
-
-        return list;
     }
 
     public List<Integer> findSubstring(String s, String[] words) {
-        List<String> segments = combinations(Arrays.asList(words));
-        var output = new HashSet<Integer>();
+        var output = new ArrayList<Integer>();
+        var totalLength = words.length * words[0].length();
 
-        for (String segment : segments) {
-            List<Integer> found = all(s, segment);
-            output.addAll(found);
+        var start = 0;
+        var end = totalLength - 1;
+
+        while (end < s.length()) {
+            String sub = s.substring(start, end + 1);
+            var results = new ArrayList<>(Collections.nCopies(words.length, false));
+
+            contains(sub, words, results);
+
+            if (results.stream().allMatch((result) -> result)) {
+                output.add(start);
+            }
+
+            start += 1;
+            end += 1;
         }
 
-        return new ArrayList<>(output);
+        return output;
     }
 }
