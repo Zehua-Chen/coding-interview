2290f41901d3fbbe7315c8a558bc01b4a50619e9
Zehua-Chen <peterchen06242000@outlook.com>
2020-10-16 21:18:24 -0500

    [feat] Add Too-Much Memory Dynamic Programming Solution

--- leetcode-1-100/032-longest-valid-parentheses/src/main/java/p032/Solution.java
+++ leetcode-1-100/032-longest-valid-parentheses/src/main/java/p032/Solution.java
@@ -1,7 +1,9 @@
 package p032;
 
 public class Solution {
-    private int longest(String s, int location, int depth) {
+    private int recursive(String s, int location, int depth) {
+        System.out.printf("location = %d, depth = %d\n", location, depth);
+
         if (location == s.length()) {
             return depth == 0 ? 0 : -1;
         }
@@ -13,11 +15,11 @@ public class Solution {
         }
 
         if (depth < 0) {
-            return 0;
+            return -1;
         }
 
         if (depth > 0) {
-            int length = longest(s, location + 1, depth);
+            int length = recursive(s, location + 1, depth);
 
             if (length == -1) {
                 return -1;
@@ -26,7 +28,7 @@ public class Solution {
             return length + 1;
         }
 
-        int length = longest(s, location + 1, 0);
+        int length = recursive(s, location + 1, 0);
 
         if (length == -1) {
             return 1;
@@ -36,13 +38,59 @@ public class Solution {
     }
 
     public int longestValidParentheses(String s) {
-        int best = 0;
+        var cache = new int[s.length() + 1][];
 
-        for (int i = 0; i < s.length(); i++) {
-            int child = longest(s, i, 0);
+        for (int i = 0; i < cache.length; i++) {
+            cache[i] = new int[s.length() + 1];
 
-            if (child > best) {
-                best = child;
+            for (int j = 0; j < s.length() + 1; j++) {
+                cache[i][j] = -1;
+            }
+        }
+
+        for (int location = s.length() - 1; location >= 0; location--) {
+            for (int depth = s.length() - 1; depth >= 0; depth--) {
+                int depthCopy = depth;
+
+                if (s.charAt(location) == '(') {
+                    depthCopy++;
+                } else {
+                    depthCopy--;
+                }
+
+                if (depthCopy < 0) {
+                    cache[location][depth] = -1;
+                    continue;
+                }
+
+                if (depthCopy > 0) {
+                    int nextLength = cache[location + 1][depthCopy];
+
+                    if (nextLength == -1) {
+                        cache[location][depth] = -1;
+                    } else {
+                        cache[location][depth] = nextLength + 1;
+                    }
+
+                    continue;
+                }
+
+                int nextLength = cache[location + 1][0];
+
+                if (nextLength == -1) {
+                    cache[location][depth] = 1;
+                    continue;
+                }
+
+                cache[location][depth] = nextLength + 1;
+            }
+        }
+
+        var best = 0;
+
+        for (int[] location : cache) {
+            if (location[0] > best) {
+                best = location[0];
             }
         }
 
