5e61d2104dd0da0bcc31c62bb9f665ebbf812279
Zehua-Chen <peterchen06242000@outlook.com>
2020-10-27 17:12:30 -0500

    [feat] Complete 007

--- others/007-erasing-zeros/src/main/java/p007/Solution.java
+++ others/007-erasing-zeros/src/main/java/p007/Solution.java
@@ -1,7 +1,44 @@
 package p007;
 
 public class Solution {
+    private int eraseZeros(String s, int start, int total) {
+        // base case
+        if (start >= s.length()) {
+            return 0;
+        }
+
+        if (start == s.length() - 1) {
+            if (s.charAt(start) == '0') {
+                return 0;
+            }
+
+            return total;
+        }
+
+        // regular cases
+        if (s.charAt(start) == '0') {
+            return eraseZeros(s, start + 1, total + 1);
+        }
+
+        // consecutive calls may return -1
+        return Math.max(total, eraseZeros(s, start + 1, total));
+    }
+
     public int eraseZeros(String s) {
-        return 0;
+        var start = 0;
+
+        for (; start < s.length(); start++) {
+            if (s.charAt(start) == '1') {
+                break;
+            }
+        }
+
+        for (; start < s.length(); start++) {
+            if (s.charAt(start) != '1') {
+                break;
+            }
+        }
+
+        return eraseZeros(s, start, 0);
     }
 }
