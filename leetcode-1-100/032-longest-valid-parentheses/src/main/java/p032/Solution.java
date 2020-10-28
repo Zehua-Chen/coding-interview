3df88f514512a80acfb601aa234e9b6ccc06695f
Zehua-Chen <peterchen06242000@outlook.com>
2020-10-27 23:44:10 -0500

    [feat] Finish 032

--- leetcode-1-100/032-longest-valid-parentheses/src/main/java/p032/Solution.java
+++ leetcode-1-100/032-longest-valid-parentheses/src/main/java/p032/Solution.java
@@ -3,32 +3,42 @@ package p032;
 import java.util.*;
 
 public class Solution {
-    private static class State {
-        public int length;
-        public int depth;
-
-        public State() {
-            this.length = -1;
-            this.depth = -1;
-        }
-    }
-
-    private State recursive(String s, int index) {
-        char c = s.charAt(index);
-        return null;
-    }
-
     public int longestValidParentheses(String s) {
-        int best = 0;
+        var maxLength = 0;
+        // we use a stack and its push and pop operation to remember the index before which a
+        // substring starts (the peek)
+        //
+        // For example, after ((), we would have [-1, 0] and i = 2, we know the substring starts
+        // at (0 + 1), and therefore have length (2 - 0) = 2
+        var stack = new Stack<Integer>();
+
+        // We need -1 so that if an entire string is well formed (which would give [-1] stack in
+        // the end), we can still calculate the length
+        stack.push(-1);
 
         for (var i = 0; i < s.length(); i++) {
-            State state = recursive(s, i);
+            char c = s.charAt(i);
 
-            if (state.length > best) {
-                best = state.length;
+            if (c == '(') {
+                // each ( can be the index before the start of a well formed substring
+                stack.push(i);
+            } else {
+                // after pop, we would know the index before the start of the well-formed substring
+                // that can be formed at i
+                stack.pop();
+
+                // in situations like ())... which would give [] at this point, we still need to
+                // be able to know the index before the start of the well formed substring "..."
+                if (stack.empty()) {
+                    stack.push(i);
+                } else {
+                    // make sure that only the longest is preserved
+                    maxLength = Math.max(maxLength, i - stack.peek());
+                }
             }
         }
 
-        return best;
+        return maxLength;
     }
 }
+
