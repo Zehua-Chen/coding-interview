ec09cbba572663c9f83d0da0ab927c1ec08d5ed8
Zehua-Chen <peterchen06242000@outlook.com>
2020-11-02 22:47:50 -0600

    [docs] Document 009

--- others/009-are-they-the-same/src/main/java/p009/Solution.java
+++ others/009-are-they-the-same/src/main/java/p009/Solution.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 
 public class Solution {
     public boolean comp(int[] as, int[] bs) {
+        // An element might appear twice; so a set does not work
         var mapA = new HashMap<Integer, Integer>();
 
         for (int a : as) {
@@ -27,6 +28,8 @@ public class Solution {
             mapA.put(b, count);
         }
 
+        // NOTE:
+        // Two arrays must have the "same" elements, which means it goes both ways
         for (int count : mapA.values()) {
             if (count != 0) {
                 return false;
