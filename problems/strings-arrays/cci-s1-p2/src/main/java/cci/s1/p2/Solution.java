06fbc578cda560305983041ee88d0cf433c4f4b2
Zehua-Chen <peterchen06242000@outlook.com>
2021-06-22 18:01:08 -0500

    [feat] Finish CCI 1.2

--- problems/strings-arrays/cci-s1-p2/src/main/java/cci/s1/p2/Solution.java
+++ problems/strings-arrays/cci-s1-p2/src/main/java/cci/s1/p2/Solution.java
@@ -1,5 +1,7 @@
 package cci.s1.p2;
 
+import java.util.*;
+
 public class Solution {
     /**
      * Given two strings, decide if the candidate is the permutation of the original
@@ -9,6 +11,22 @@ public class Solution {
      * @return true if candidate is the permutatino of original
      */
     public boolean isPermutation(String original, String candidate) {
-        return false;
+        char[] originalChars = original.toCharArray();
+        char[] candidateChars = candidate.toCharArray();
+
+        if (originalChars.length != candidateChars.length) {
+            return false;
+        }
+
+        Arrays.sort(originalChars);
+        Arrays.sort(candidateChars);
+
+        for (int i = 0; i < originalChars.length; i++) {
+            if (originalChars[i] != candidateChars[i]) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
