b3fd03384a48ad6188fe4888f16bd90aa66abf68
Zehua-Chen <peterchen06242000@outlook.com>
2021-06-24 21:16:25 -0500

    [feat] Finish 1.6

--- problems/strings-arrays/cci-s1-p6/README.md
+++ problems/strings-arrays/cci-s1-p6/README.md
@@ -4,4 +4,6 @@
 Perform string compression using the counts of repeated characters. If the compressed string is not
 smaller than the original string, return the original string.
 
+- Assume the string consists of only upper case and lower case letters
+
 ## Questions that Should be Asked
--- problems/strings-arrays/cci-s1-p6/src/main/java/cci/s1/p6/BaselineSolution.java
+++ problems/strings-arrays/cci-s1-p6/src/main/java/cci/s1/p6/BaselineSolution.java
@@ -2,6 +2,32 @@ package cci.s1.p6;
 
 public class BaselineSolution implements Solution {
     public String compress(String raw) {
-        return null;
+        // a slightly different approach is to
+        // 1. count the resulting compression length first
+        // 2. decide if compression is needed
+        // 3. if compression is needed, reserve enough space in string builder
+        int i = 0;
+        var builder = new StringBuilder();
+
+        while (i < raw.length()) {
+            char head = raw.charAt(i++);
+            int count = 1;
+
+            while (i < raw.length() && head == raw.charAt(i)) {
+                count++;
+                i++;
+            }
+
+            builder.append(head);
+            builder.append(count);
+        }
+
+        String compressed = builder.toString();
+
+        if (compressed.length() >= raw.length()) {
+            return raw;
+        }
+
+        return compressed;
     }
 }
