27a79b55015f0af85c4c21d19131dbefe3dc4e3e
Zehua-Chen <peterchen06242000@outlook.com>
2021-06-29 16:09:31 -0500

    [feat] Add Baseline Solution

--- problems/strings-arrays/cci-s1-p8/README.md
+++ problems/strings-arrays/cci-s1-p8/README.md
@@ -1,7 +1,7 @@
 # Cracking the Coding Interview 1.8
 
-**Rotate matrix**: write an algorithm such that if an element is an `MxN` matrix is 0, its entire
-row and column are set to 0.
+**Zero matrix**: write an algorithm such that if an element is an `MxN` matrix is 0, its entire row
+and column are set to 0.
 
 ## Questions that Should be Asked
 
--- problems/strings-arrays/cci-s1-p8/src/main/java/cci/s1/p8/BaselineSolution.java
+++ problems/strings-arrays/cci-s1-p8/src/main/java/cci/s1/p8/BaselineSolution.java
@@ -1,6 +1,32 @@
 package cci.s1.p8;
 
+import java.util.*;
+
 public class BaselineSolution implements Solution {
     @Override
-    public void setZero(int[][] input) {}
+    public void setZero(int[][] input) {
+        var rows = new HashSet<Integer>();
+        var cols = new HashSet<Integer>();
+
+        for (int y = 0; y < input.length; y++) {
+            for (int x = 0; x < input[y].length; x++) {
+                if (input[y][x] == 0) {
+                    rows.add(y);
+                    cols.add(x);
+                }
+            }
+        }
+
+        for (Integer row : rows) {
+            for (int x = 0; x < input[0].length; x++) {
+                input[row][x] = 0;
+            }
+        }
+
+        for (Integer col : cols) {
+            for (int y = 0; y < input.length; y++) {
+                input[y][col] = 0;
+            }
+        }
+    }
 }
