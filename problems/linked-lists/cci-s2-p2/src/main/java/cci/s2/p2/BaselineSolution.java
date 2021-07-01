8b728842d875ee88f5a8cc98107ee277caf988a3
Zehua-Chen <peterchen06242000@outlook.com>
2021-06-30 22:07:58 -0500

    [feat] Add Baseline Solution to CII S2 P2

--- problems/linked-lists/cci-s2-p2/src/main/java/cci/s2/p2/BaselineSolution.java
+++ problems/linked-lists/cci-s2-p2/src/main/java/cci/s2/p2/BaselineSolution.java
@@ -3,8 +3,27 @@ package cci.s2.p2;
 import utils.lists.Node;
 
 public class BaselineSolution implements Solution {
+    int length(Node<Integer> head) {
+        int count = 0;
+
+        while (head != null) {
+            head = head.getNext();
+            count++;
+        }
+
+        return count;
+    }
+
     @Override
     public Integer findLast(Node<Integer> head, int k) {
-        return -1;
+        int length = length(head);
+        int index = 0;
+
+        while (index < (length - k)) {
+            head = head.getNext();
+            index++;
+        }
+
+        return head.getValue();
     }
 }
